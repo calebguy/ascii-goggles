c65ac82c0453f7d1f5fd6ccf26acc9aaf339e3bd
caleb <caleb@syndicate.io>
2026-03-09 10:33:27 -0500

    cam button

--- server.ts
+++ server.ts
@@ -1,14 +1,15 @@
 Bun.serve({
-  port: 3000,
-  async fetch(req) {
-    const url = new URL(req.url);
-    const path = url.pathname === "/" ? "/index.html" : url.pathname;
-    const file = Bun.file("." + path);
-    if (await file.exists()) {
-      return new Response(file);
-    }
-    return new Response("Not found", { status: 404 });
-  },
+	port: 3000,
+	hostname: "0.0.0.0",
+	async fetch(req) {
+		const url = new URL(req.url);
+		const path = url.pathname === "/" ? "/index.html" : url.pathname;
+		const file = Bun.file("." + path);
+		if (await file.exists()) {
+			return new Response(file);
+		}
+		return new Response("Not found", { status: 404 });
+	},
 });
 
 console.log("Seeing ASCII running at http://localhost:3000");
